004a26b3fe8b663281e3e8f27fee87e024d1feb1
Muthukumaran Ganapathy <muthukumaran.ganapathy@nurturebox.ai>
2024-03-18 17:34:45 +0530

    Binary search problems added

---
+++ Arrays/binary-search/kth_positive_missing_array.js
@@ -0,0 +1,40 @@
+/* 
+  Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+  Return the kth positive integer that is missing from this array. 
+
+  Example 1:
+
+  Input: arr = [2,3,4,7,11], k = 5
+  Output: 9
+  Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+*/
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+
+// Intuition - check the arr[m] postion(index)
+var findKthPositive = function (arr, k) {
+  let s = 0,
+    e = arr.length - 1;
+
+  while (s <= e) {
+    let m = Math.floor(s + (e - s) / 2);
+
+    let missing = arr[m] - (m + 1);
+    console.log("Missing", { s, e, m, missing });
+    if (missing < k) {
+      s = m + 1;
+    } else {
+      e = m - 1;
+    }
+  }
+  return s + k;
+};
+
+const arr = [1, 2, 3, 4],
+  k = 2;
+
+console.log("Missing positive integer", findKthPositive(arr, k));
---
+++ Arrays/binary-search/peak_element.js
@@ -0,0 +1,45 @@
+/* A peak element is an element that is strictly greater than its neighbors.
+
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+
+You must write an algorithm that runs in O(log n) time. */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+  let n = nums.length - 1;
+
+  if (nums.length === 1) return 0;
+  if (nums[0] > nums[1]) return 0;
+  if (nums[n] > nums[n - 1]) return n;
+  const peakEl = peakElement(nums);
+  return peakEl;
+};
+
+var peakElement = function (arr) {
+  let s = 0,
+    e = arr.length - 1;
+
+  while (s <= e) {
+    let m = Math.floor(s + (e - s) / 2);
+
+    if (arr[m] > arr[m - 1] && arr[m] > arr[m + 1]) {
+      return m;
+    }
+
+    if (arr[m] < arr[m + 1]) {
+      s = m + 1;
+    } else if (arr[m] > arr[m + 1]) {
+      e = m - 1;
+    }
+  }
+  return -1;
+};
+let nums = [1, 2, 4, 6];
+nums = [11, 13, 15, 17, 20];
+
+console.log("Peak element", findPeakElement(nums));
