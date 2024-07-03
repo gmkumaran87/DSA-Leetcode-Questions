b9e66e713ed2c362795c31e64014003f8a3cb432
Muthukumaran <gmkumaran87@gmail.com>
2024-07-03 20:13:19 +0530

    Sorting problem added

---
+++ Arrays/sorting/easy/sort_array_parity.js
@@ -0,0 +1,34 @@
+/**
+ * Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+   Return any array that satisfies this condition.
+ 
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function (nums) {
+  let s = 0,
+    e = nums.length - 1;
+
+  while (s <= e) {
+    if (nums[s] % 2 !== 0 && nums[e] % 2 === 0) {
+      let temp = nums[e];
+      nums[e] = nums[s];
+      nums[s] = temp;
+      s++;
+      e--;
+      //   console.log("FIrst", temp, nums, s, e);
+    } else if (nums[s] % 2 === 0) {
+      s++;
+    } else if (nums[e] % 2 !== 0) {
+      e--;
+    }
+  }
+  return nums;
+};
+
+const nums = [3, 1, 2, 4];
+
+console.log("Sorting array by parity...", sortArrayByParity(nums));
---
+++ Arrays/sorting/easy/sort_array_parity_2.js
@@ -0,0 +1,47 @@
+/**
+ * Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+   Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+   Return any answer array that satisfies this condition.
+ */
+
+/* Solution 
+ * 1. Create two array with odd numbers and even numbers from source array
+      Create output array and insert them alternatively from odd and even array.  
+
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParityII = function (nums) {
+  let oddArr = [],
+    evenArr = [],
+    outArr = [];
+
+  for (let num of nums) {
+    if (num % 2 === 0) {
+      evenArr.push(num);
+    } else {
+      oddArr.push(num);
+    }
+  }
+
+  console.log(evenArr, oddArr);
+  let j = 0,
+    k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      outArr.push(evenArr[j]);
+      j++;
+    } else {
+      outArr.push(oddArr[k]);
+      k++;
+    }
+  }
+  return outArr;
+};
+
+const nums = [4, 2, 5, 7];
+
+console.log(sortArrayByParityII(nums));
